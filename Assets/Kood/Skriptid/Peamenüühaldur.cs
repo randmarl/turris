6b0912991249f355db06d36ed90172689270db0f
randmarl <marleen.rand@gmail.com>
2026-01-29 10:58:45 +0200

    tasemete vaade valmis

--- Assets/Kood/Skriptid/Peamenüühaldur.cs
+++ Assets/Kood/Skriptid/Peamenüühaldur.cs
@@ -1,21 +1,40 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Peamenüühaldur : MonoBehaviour
 {
+    [Header("UI paneelid")]
+    [SerializeField] private GameObject peamenüüPaneel;
+    [SerializeField] private GameObject tasemetePaneel;
+
+    [Header("Stseenid")]
+    [SerializeField] private string tase1Stseen = "Level1";
+
     public void AvaTasemed()
     {
-        Debug.Log("Tasemed avatud");
+        peamenüüPaneel.SetActive(false);
+        tasemetePaneel.SetActive(true);
+    }
+
+    public void TagasiPeamenüüsse()
+    {
+        tasemetePaneel.SetActive(false);
+        peamenüüPaneel.SetActive(true);
+    }
+
+    public void AvaTase1()
+    {
+        SceneManager.LoadScene(tase1Stseen);
     }
 
     public void AvaOpetus()
     {
-        Debug.Log("Õpetus avatud");
+        Debug.Log("Õpetus avaneb.");
     }
 
     public void Välju()
     {
         Application.Quit();
-
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #endif
